c2efac7d6f13dc845aae23d8cd0a02423f601032
Jonas Drotleff <jonas.drotleff@gmail.com>
2020-02-17 13:49:36 +0100

    Fix matomo script

--- _includes/matomo.js
+++ _includes/matomo.js
@@ -1,11 +1,10 @@
-w._paq || [];
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+var _paq = window._paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-  var u="//matomo.desk-lab.de/";
-  _paq.push(['setTrackerUrl', u+'matomo.php']);
-  _paq.push(['setSiteId', '2']);
-  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    var u="//matomo.desk-lab.de/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '2']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 })();
